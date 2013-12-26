bae198826f84f15afc8a22a78aa0665439b0f575
Ben Cox <Ben@Benjojo.co.uk>
2013-12-26 14:11:34 +0000

    Some basic layout of how the server will work

--- server/main.go
+++ server/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"github.com/codegangsta/martini"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("I am a webserver get me out of here.")
+	UpChan := make(chan []byte)
+	DoChan := make(chan []byte)
+	go TCPSocket(UpChan, DoChan)
+	// Now that the TCP waiter is setup. lets start the HTTP sevrer
+	m := martini.Classic()
+	// m.Use(EnforceHTTPAuth)
+	m.Get("/", Welcome)
+	m.Run()
+}
+
+func TCPSocket(UpChan chan []byte, DoChan chan []byte) {
+
+}
+
+func Welcome(rw http.ResponseWriter, req *http.Request) string {
+	return "Why Howdy there"
 }
