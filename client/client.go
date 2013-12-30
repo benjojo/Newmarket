c9a9515bc865ba8d4a0ad337bc9a35282c9c67cb
Ben Cox <Ben@Benjojo.co.uk>
2013-12-30 21:19:03 +0000

    added cli.go and some setup around that

--- client/client.go
+++ client/client.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
-	// "net"
+	"github.com/codegangsta/cli"
+	"os"
 )
 
 func main() {
-	fmt.Println("Newmarket Client.")
+	app := cli.NewApp()
+	app.Name = "Newmarket Client"
+	app.Usage = "A Client to the Newmarket HTTP Tunnel server"
+	app.Action = func(c *cli.Context) {
+		fmt.Println("Starting Newmarket client")
+		StartTunnel(c.String("url"), c.String("port"))
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{"url, u", "http://localhost:3000", "the URL of the Newmarket server"},
+		cli.StringFlag{"port, p", "3001", "The port you want to listen on"},
+	}
+	app.Run(os.Args)
+}
+
+func StartTunnel(URL string, Port string) {
+	fmt.Printf("The settings are \n\nURL:%s\nListening Port:%s\n", URL, Port)
 }
