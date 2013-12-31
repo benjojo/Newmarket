bacb128f0e67a30a2ed2b3ff3099eb9d60a107fa
Ben Cox <ben@benjojo.co.uk>
2013-12-31 16:23:30 +0000

    DailUpWards looks good, now to test...

--- client/client.go
+++ client/client.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -65,12 +66,41 @@ func HandleTunConnection(conn net.Conn, URL string, Port int64) {
 	sessiontoken := string(sessdata)
 	fmt.Println("Session tokean obtained:", sessiontoken)
 	// Okay so we now have our session token.
-	go DialUpWards(URL+"/session/"+sessiontoken, conn)
+	go DialUpWards(URL, sessiontoken, conn)
 	go DialDownWards(URL+"/session/"+sessiontoken, conn)
 }
 
-func DialUpWards(URL string, conn net.Conn) {
-
+func DialUpWards(URL string, sessiontoken string, conn net.Conn) {
+	// because go can't do what I am about to do I am going to
+	// #yolo my own HTTP code fora bit :v
+	URL = strings.Replace(URL, "http://", "", 1)
+	// WARNING THIS CODE WONT HELP YOU AGAINST SUB FOLDER URLS
+	// NEED TO BE REFACTORED OR HELL, JUST DONE PROPERALLY AND NOT
+	// INSANE LIKE THIS ONE IS.
+	conn2, err := net.Dial("tcp", URL)
+	if err != nil {
+		fmt.Errorf("Woah wtf, I tried to dial up and I got a error! %s", err)
+		conn.Close()
+		return
+	}
+	HTTPRequest := fmt.Sprintf("POST /session/%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: Newmarket\r\nContent-Length: 9999999\r\n\r\n", sessiontoken, URL)
+	conn2.Write([]byte(HTTPRequest))
+	buf := make([]byte, 25565)
+	for {
+		read, e := conn.Read(buf)
+		if e != nil {
+			fmt.Errorf("Upstream broke down for reason %s", e)
+			conn.Close()
+			return
+		}
+		_, e = conn2.Write(buf[0:read])
+		if e != nil {
+			fmt.Errorf("Tried to write data to remotesocket and it broke: %s", e)
+			conn.Close()
+			conn2.Close()
+			return
+		}
+	}
 }
 
 func DialDownWards(URL string, conn net.Conn) {
