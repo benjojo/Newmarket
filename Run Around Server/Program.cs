dbb5cf30bc512bb7d07d11070e74abd81c4cdb06
Ben Cox <Ben@Benjojo.co.uk>
2014-01-04 20:22:33 +0000

    Adding first version of runaround

---
+++ Run Around Server/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+namespace Run_Around_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPEndPoint UplinkEP = new IPEndPoint(IPAddress.Any, 8443);
+            IPEndPoint DownLinkEP = new IPEndPoint(IPAddress.Any, 8880);
+            Socket UplinkSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket DownLinkSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            UplinkSock.Bind(UplinkEP);
+            UplinkSock.Listen(10);
+            DownLinkSock.Bind(DownLinkEP);
+            DownLinkSock.Listen(10);
+            
+            // Both ends setup.
+
+            Socket DownlinkSockR = DownLinkSock.Accept();
+            Console.WriteLine("Downlink online.");
+            Socket UplinkSockR = UplinkSock.Accept();
+            Console.WriteLine("Uplink online. Making Connection to Final Endpoint.");
+
+            Socket ProxyEndSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ProxyEndEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 22);
+            /*
+            NetworkStream DownLinkNS = new NetworkStream(DownlinkSockR);
+            NetworkStream UpLinkNS = new NetworkStream(UplinkSockR);
+            StreamReader DownLinkSr = new StreamReader(DownLinkNS);
+            StreamWriter DownLinkSw = new StreamWriter(DownLinkNS);
+            StreamReader UpLinkSr = new StreamReader(UpLinkNS);*/
+            Console.WriteLine("Made Streams");
+            while (true)
+            {
+                int DLSA = DownlinkSockR.Available;
+                if (DLSA != 0){
+                    Thread.Sleep(33);
+                    Console.WriteLine("Headers are {0} bytes long", DLSA);
+                    byte[] Inbound = new byte[DLSA];
+                    DownlinkSockR.Receive(Inbound);
+                    break;
+                }
+            }
+            Console.WriteLine("Got the Clients (bullshit) headers");
+            string ResponceHeaders = @"HTTP/1.0 200 OK
+Vary: Accept-Encoding
+Content-Type: application/octet-stream
+Accept-Ranges: bytes
+Last-Modified: Tue, 11 Dec 2012 10:32:35 GMT
+Content-Length: 1073741824
+Date: Wed, 30 Jan 2013 10:59:29 GMT
+Server: SeemsLegitCo/1
+Proxy-Connection: keep-alive
+
+";
+            DownlinkSockR.Send(Encoding.ASCII.GetBytes(ResponceHeaders));
+            Console.WriteLine("Sent my headers");
+            // OKay So the proxy thinks we are legit as fuck now.
+            // Lets connect to the ProxyEndpoint.
+            ProxyEndSock.Connect(ProxyEndEP);
+            Console.WriteLine("We have connected to our localend.");
+            NetworkStream ProxyEndNS = new NetworkStream(ProxyEndSock);
+            StreamReader ProxyEndSR = new StreamReader(ProxyEndNS);
+            StreamWriter ProxyEndSW = new StreamWriter(ProxyEndNS);
+
+            while (true)
+            {
+                Thread.Sleep(33); // hacks hacks lazyness etc
+                int UplinkAvail = UplinkSockR.Available;
+                if (UplinkAvail != 0)
+                {
+                    // read and send
+                    byte[] Inbound = new byte[UplinkAvail];
+                    UplinkSockR.Receive(Inbound);
+                    ProxyEndSock.Send(Inbound);
+                }
+                int ProxyAvail = ProxyEndSock.Available;
+                if (ProxyAvail != 0)
+                {
+                    // read and send.
+                    byte[] Inbound = new byte[ProxyAvail];
+                    ProxyEndSock.Receive(Inbound);
+                    DownlinkSockR.Send(Inbound);
+                }
+            }
+            
+        }
+    }
+}
---
+++ RunAround/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace RunAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string IP = "78.129.218.173";
+            string UpLinkHeaders = @"GET / HTTP/1.1
+Host: "+IP+@"
+Connection: keep-alive
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17
+DNT: 1
+Accept-Encoding: gzip,deflate,sdch
+Accept-Language: en-US,en;q=0.8
+Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
+
+";
+            IPEndPoint ListenEndPoint = new IPEndPoint(IPAddress.Any,2222);
+            Socket ListenSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            ListenSocket.Bind(ListenEndPoint);
+            ListenSocket.Listen(10);
+            Console.WriteLine("Ready. Connect on {0}",ListenEndPoint.Port);
+            Socket LocalClient = ListenSocket.Accept();
+
+            // Now we have the local connection we need to knock at the other servers door.
+            Socket RemoteDownLink = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket RemoteUpLink = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ProxyDLEnd = new IPEndPoint(IPAddress.Parse(IP), 80);
+            IPEndPoint ProxyULEnd = new IPEndPoint(IPAddress.Parse(IP), 443);
+
+            try
+            {
+                RemoteDownLink.Connect(ProxyDLEnd);
+                RemoteUpLink.Connect(ProxyULEnd);
+                Console.WriteLine("Both Endpoints connected.\r\nSending Headers on HTTP end");
+                RemoteDownLink.Send(Encoding.ASCII.GetBytes(UpLinkHeaders));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.StackTrace);
+                Console.ReadLine();
+            }
+            // Remote End bits
+            /*NetworkStream DownLinkStream = new NetworkStream(RemoteDownLink);
+            StreamReader DownLinkInboundStream = new StreamReader(DownLinkStream);
+            NetworkStream UpLinkStream = new NetworkStream(RemoteUpLink);
+            StreamWriter UpLinkWriteStream = new StreamWriter(UpLinkStream);*/
+            // Local End Bits
+            /*NetworkStream LocalEndStream = new NetworkStream(LocalClient);
+            StreamReader LocalEndRead = new StreamReader(LocalEndStream);
+            StreamWriter LocalEndWrite = new StreamWriter(LocalEndStream);*/
+
+            // We need to wait for the headers that the downlink sends to finish.
+            while (true)
+            {
+                Thread.Sleep(33); // ADSL anyway.
+                int DLSA = RemoteDownLink.Available;
+                if (DLSA != 0)
+                {
+                    Console.WriteLine("Headers are {0} bytes long", DLSA);
+                    byte[] Inbound = new byte[DLSA];
+                    RemoteDownLink.Receive(Inbound);
+                    break;
+                }
+                //if (line == "") { break; } // HTTP sends a blank line at the end.
+            }
+            Console.WriteLine("Got Servers headers...");
+            // Ok the headers are done.
+            while (true)
+            {
+                Thread.Sleep(33); // ADSL anyway.
+                int LESA = LocalClient.Available;
+                if (LESA != 0)
+                {
+                    Console.WriteLine("-> {0}", LESA);
+                    byte[] Inbound = new byte[LESA]; 
+                    LocalClient.Receive(Inbound);
+                    RemoteUpLink.Send(Inbound, LESA, SocketFlags.None);
+                }
+                int DLSA = RemoteDownLink.Available;
+                if (DLSA != 0)
+                {
+                    Console.WriteLine("<- {0}", DLSA);
+                    byte[] Inbound = new byte[DLSA];
+                    RemoteDownLink.Receive(Inbound);
+                    LocalClient.Send(Inbound, DLSA, SocketFlags.None);
+                }
+            }
+
+        }
+    }
+}
